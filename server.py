50e8c1d3406a871cd1afca0ea58e0654d1b61e1d
Liseth Cardozo <cardozoliseth@gmail.com>
2017-08-04 23:55:27 -0700

    Refactor moving out the common code

--- server.py
+++ server.py
@@ -12,28 +12,23 @@ def index():
     else:
         if session["building"] == "farm":
             gold = random.randrange(10, 21)
-            time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            visited_place = "Earned " + str(gold) + " golds from the " + session["building"] +"!" + " (" + time + ")"
         elif session["building"] == "cave":
             gold = random.randrange(5, 11)
-            time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            visited_place = "Earned " + str(gold) + " golds from the " + session["building"] +"!" + " (" + time + ")"
         elif session["building"] == "house":
-            time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             gold = random.randrange(2, 6)
-            visited_place = "Earned " + str(gold) + " golds from the " + session["building"] +"!" + " (" + time + ")"
         elif session["building"] == "casino":
-            time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             take_or_earn = random.randrange(0, 2)
             if take_or_earn == 0:
                 gold = random.randrange(0, 51)
-                visited_place = "Earned " + str(gold) + " golds from the " + session["building"] +"!" + " (" + time + ")"
             else:
-                gold = random.randrange(0, 51)
-                time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                visited_place = "Enter a casino and lost " + str(gold) + " golds OOUCHHH!"  + " (" + time + ")"
+                gold = -random.randrange(0, 51)
         elif session["building"] == '':
             return render_template("index.html", visited_place = session["visited_places"])
+        time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        if gold >= 0:
+            visited_place = "Earned " + str(gold) + " golds from the " + session["building"] +"!" + " (" + time + ")"
+        else:
+            visited_place = "Enter a " + session["building"] + " and lost " + str(-gold) + " golds OOUCHHH!"  + " (" + time + ")"
         session['visited_places'] += [visited_place]
         session['building'] = ''
         return render_template("index.html", visited_place = session["visited_places"])
